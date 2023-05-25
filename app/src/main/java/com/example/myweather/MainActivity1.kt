61b3c48c14255c581becf0dcf5764540c27360ea
Ruthgitau <gitaur72@gmail.com>
2023-05-25 13:10:23 +0300

    weather app

--- app/src/main/java/com/example/myweather/MainActivity1.kt
+++ app/src/main/java/com/example/myweather/MainActivity1.kt
@@ -5,15 +5,16 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 
-class MainActivity2 : ComponentActivity() {
+class MainActivity1 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity2_main)
-        val viewbutton: Button = findViewById(R.id.button3)
-        viewbutton.setOnClickListener {
-            val intent = Intent(this, MainActivity3::class.java)
+        setContentView(R.layout.activity1_main)
+        val signupbutton: Button = findViewById(R.id.button2)
+        signupbutton.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
-
     }
 }
+
+
