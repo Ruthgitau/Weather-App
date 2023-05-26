4e11356b0819c51da522ebf41f07c63d7e06b3ff
Ruthgitau <gitaur72@gmail.com>
2023-05-26 14:34:35 +0300

    weather app

--- app/src/main/java/com/example/myweather/MainActivity1.kt
+++ app/src/main/java/com/example/myweather/MainActivity1.kt
@@ -2,19 +2,26 @@ package com.example.myweather
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity1 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity1_main)
-        val signupbutton: Button = findViewById(R.id.button2)
-        signupbutton.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
+
+    val openactivity:Button=findViewById(R.id.button2)
+        openactivity.setOnClickListener{
+            val intent=Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
+
+
     }
+
 }
+
 
 
--- app/src/main/java/com/example/myweather/MainActivity3.kt
+++ app/src/main/java/com/example/myweather/MainActivity3.kt
@@ -1,6 +1,9 @@
 package com.example.myweather
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,8 +16,50 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.myweather.ui.theme.MyWeatherTheme
 
 class MainActivity3 : ComponentActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity3_main)
+
+
+        //toast 1
+       val toastbtn:Button=findViewById(R.id.button4)
+        toastbtn.setOnClickListener{
+            val mytoast=Toast.makeText(applicationContext,"hello toast", Toast.LENGTH_SHORT)
+            mytoast.show()
+        }
+
+        //toast 2
+
+        val button:Button=findViewById(R.id.button7)
+        button.setOnClickListener {
+            button.text="you clicked me"
+            Toast.makeText(this@MainActivity3, "click", Toast.LENGTH_SHORT)
+                .show()
+
+        }
+
+        //button on long click listener
+        val longbtn:Button=findViewById(R.id.button8)
+        longbtn.setOnLongClickListener{
+            longbtn.text="long pressed"
+            true
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
+
+
+
+
+
+
