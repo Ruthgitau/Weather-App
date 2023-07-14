d7b2b0893c71d157f243cefc56cc9a648e5a1e49
Ruthgitau <gitaur72@gmail.com>
2023-07-14 10:53:09 +0300

    weather app

--- app/src/main/java/com/example/myweather/MainActivity1.kt
+++ app/src/main/java/com/example/myweather/MainActivity1.kt
@@ -1,13 +1,16 @@
 package com.example.myweather
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class MainActivity1 : ComponentActivity() {
+    @SuppressLint("SuspiciousIndentation", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity1_main)
@@ -16,6 +19,16 @@ class MainActivity1 : ComponentActivity() {
         openactivity.setOnClickListener{
             val intent=Intent(this, MainActivity2::class.java)
             startActivity(intent)
+        }
+
+
+
+        val button:Button=findViewById(R.id.button5)
+        button.setOnClickListener {
+            button.text="LOGIN"
+            Toast.makeText(this@MainActivity1, "Successfully Logged In", Toast.LENGTH_SHORT)
+                .show()
+
         }
 
 
